8457fe78b6b0df36f1295840e6c53d5f1d254aa1
xiaoxiae <tomas@slama.dev>
2025-08-22 13:12:38 +0200

    code

--- src/project.ts
+++ src/project.ts
@@ -1,11 +1,12 @@
 import { makeProject } from '@motion-canvas/core';
 
 import basic from './scenes/basic?scene';
+import code from './scenes/code?scene';
 import text from './scenes/text?scene';
 
 import './global.css';
 
 export default makeProject({
   experimentalFeatures: true,
-  scenes: [basic, text],
+  scenes: [basic, text, code],
 });
