c9183d294c08b84b45d88e14931ceda1056f7b56
Václav Volhejn <vaclav.volhejn@gmail.com>
2025-11-16 23:51:06 +0100

    Make equation animations to match script

--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,11 @@
 import { makeProject } from '@motion-canvas/core';
 
+import equations from './scenes/examples/equations?scene';
 import table from './scenes/examples/quicksort/table?scene';
 
 import './global.css';
 
 export default makeProject({
   experimentalFeatures: true,
-  scenes: [table],
+  scenes: [equations],
 });
--- src/scenes/examples/equations.tsx
+++ src/scenes/examples/equations.tsx
@@ -1,7 +1,8 @@
-import { Layout, makeScene2D, Rect } from '@motion-canvas/2d';
+import { Circle, Img, Layout, makeScene2D, Node, Rect } from '@motion-canvas/2d';
 import { Code } from '@motion-canvas/2d/lib/components';
 import {
   all,
+  beginSlide,
   chain,
   Color,
   createRef,
@@ -14,6 +15,7 @@ import {
   waitFor,
 } from '@motion-canvas/core';
 
+import desmosPlotWrongEquation from '../../assets/desmos-plot-wrong-equation.png';
 import { ShikiHighlighter } from '../../components/Shiki';
 import { Solarized } from '../../utilities/color';
 import { appear } from '../../utilities/creation';
@@ -31,17 +33,16 @@ const juliaHighlighter = new ShikiHighlighter({
 
 export default makeScene2D(function* (view) {
   view.fill(Solarized.background);
+  yield* beginSlide('simple equation');
+  const camera = <Node />;
+  view.add(camera);
 
   const simpleLayout = createRef<Layout>();
   const simpleEquation = createRef<PolyLatex>();
   const simpleText = createRef<PolyTxt>();
-  const complexLayout = createRef<Layout>();
-  const complexEquation = createRef<PolyLatex>();
-  const complexText = createRef<PolyTxt>();
-  const juliaCode = createRef<Code>();
 
   // Simple equation layout - starts in center
-  view.add(
+  camera.add(
     <Layout
       ref={simpleLayout}
       layout
@@ -49,7 +50,11 @@ export default makeScene2D(function* (view) {
       gap={80}
       alignItems={'center'}
     >
-      <PolyLatex ref={simpleEquation} tex={'(n + 1)^2 = n^2 + 2n + 1'} fontSize={60} />
+      <PolyLatex
+        ref={simpleEquation}
+        tex={'{{(x - 1)}}^2 = {{x^2}} - 2{{x}} + {{1}}'}
+        fontSize={80}
+      />
       <PolyTxt
         ref={simpleText}
         text={'Simple \\😊/'}
@@ -60,33 +65,7 @@ export default makeScene2D(function* (view) {
     </Layout>,
   );
 
-  view.add(createShadow(simpleText));
-
-  // Complex equation layout - starts off-screen to the right
-  view.add(
-    <Layout
-      ref={complexLayout}
-      layout
-      direction={'column'}
-      gap={80}
-      alignItems={'center'}
-      opacity={0}
-    >
-      <PolyLatex
-        ref={complexEquation}
-        tex={
-          '2\\sum_{k=1}^n k^7 = \\left(\\sum_{k=1}^n k\\right)^2 - 3\\left(\\sum_{k=1}^n k^2\\right)^2 + 4\\left(\\sum_{k=1}^n k^3\\right)^2'
-        }
-        fontSize={50}
-      />
-      <PolyTxt
-        ref={complexText}
-        text={'Difficult /😫\\'}
-        fontSize={80}
-        fill={Solarized.red}
-      />
-    </Layout>,
-  );
+  camera.add(createShadow(simpleText));
 
   // Start with the simple equation in the center
   // First, show the simple equation
@@ -98,33 +77,216 @@ export default makeScene2D(function* (view) {
   yield* appear(simpleText);
 
   yield* waitFor(1);
-  view.add(createShadow(complexText));
 
-  // Shift simple equation to the left and bring in the complex equation
+  yield* beginSlide('equation 2');
+
+  // (x - 1)^3 = x^3 + 3x^2 - 3x - 1
   yield* all(
+    simpleEquation().tex('{{(x - 1)}}^3 = {{x^3}} + 3{{x^2}} - 3{{x}} - {{1}}', 1),
+    // simpleText().opacity(0, 1),
+    simpleText().text('?', 1),
+  );
+
+  yield* beginSlide('equation 2 is wrong');
+
+  const equation2Wrong = createRef<Node>();
+
+  camera.add(
+    <Node opacity={0} ref={equation2Wrong}>
+      <Circle stroke={Solarized.red} size={100} lineWidth={5} position={[15, -80]} />
+      <Circle stroke={Solarized.red} size={100} lineWidth={5} position={[275, -80]} />
+    </Node>,
+  );
+
+  camera.add(equation2Wrong);
+
+  yield* all(
+    simpleEquation().tex('{{(x - 1)}}^3 = {{x^3}} + 3{{x^2}} - 3{{x}} - {{1}}', 1),
+    // simpleText().opacity(0, 1),
+    simpleText().text("Doesn't hold", 1),
+    simpleText().fill(Solarized.red, 1),
+    equation2Wrong().opacity(1, 1),
+  );
+
+  yield* beginSlide('complicated equation');
+
+  const complicatedLayout = createRef<Layout>();
+  const complicatedEquation = createRef<PolyLatex>();
+  const complicatedText = createRef<PolyTxt>();
+
+  // complicated equation layout - starts off-screen to the right
+  camera.add(
+    <Layout
+      ref={complicatedLayout}
+      layout
+      direction={'column'}
+      gap={80}
+      alignItems={'center'}
+      opacity={0}
+    >
+      <PolyLatex
+        ref={complicatedEquation}
+        tex={
+          '(x^2-{{3}})({{x^6}}+{{x^2}}+1) {{= x^2 (x^2 ((x^2 - 3) x^2 + 1) - 2) - 3}}'
+        }
+        fontSize={50}
+      />
+      <PolyTxt
+        ref={complicatedText}
+        text={' '}
+        fontSize={80}
+        fill={Solarized.blue}
+        opacity={0}
+      />
+    </Layout>,
+  );
+
+  // Shift simple equation to the left and bring in the complicated equation
+  yield* all(
+    equation2Wrong().opacity(0, 0.5),
     simpleLayout().position(simpleLayout().position().addX(-1400), 1),
     simpleLayout().scale(0.5, 1),
-    complexLayout().position([1600, 0]).position([0, 0], 1),
-    complexLayout().scale(0.5).scale(1, 1),
+    complicatedLayout().position([1600, 0]).position([0, 0], 1),
+    complicatedLayout().scale(0.5).scale(1, 1),
     delay(
       0.5,
-      all(
-        complexLayout().opacity(1, 0.1),
-        complexEquation().write(2),
-        complexText().opacity(0).opacity(1, 1),
-      ),
+      all(complicatedLayout().opacity(1, 0.1), complicatedEquation().write(2)),
     ),
   );
 
-  yield* waitFor(2);
+  yield* beginSlide('complicated equation expanded');
 
-  // Hide the "Difficult" text and show Julia code
-  yield* complexText().opacity(0, 0.5, easeInOutCubic);
+  yield* complicatedEquation().tex(
+    'x^8 - {{3}} {{x^6}} + x^4 - 2 {{x^2}} - {{3}} {{= x^2 (x^2 ((x^2 - 3) x^2 + 1) - 2) - 3}}',
+    1,
+  );
+  yield* beginSlide('complicated equation expanded 2');
+
+  // Make the groups only on the right side to make the movement clearer
+  complicatedEquation().tex(
+    'x^8 - 3 x^6 + x^4 - 2 x^2 - 3 {{=}} x^2 (x^2 (({{x^2}} - {{3}}) {{x^2}} + 1) {{- 2}}) {{- 3}}',
+  );
+
+  yield* complicatedEquation().tex(
+    'x^8 - 3 x^6 + x^4 - 2 x^2 - 3 {{=}} x^8 - {{3}} x^6 + x^4 {{- 2}} {{x^2}} {{- 3}}',
+    1,
+  );
+
+  yield* beginSlide('complicated equation expanded 3');
+
+  yield* all(
+    complicatedEquation().tex('0 {{=}} 0', 1),
+    complicatedEquation().scale(2, 1),
+  );
+
+  yield* beginSlide('exponential growth of equation');
+
+  yield* all(
+    complicatedEquation().scale(2, 1),
+    complicatedEquation().tex('{{(x-2)^7}}', 1),
+  );
+
+  yield* beginSlide('exponential growth of equation p2');
+
+  yield* all(
+    complicatedEquation().tex(
+      '{{(x-2)^7}} = x^7 - 14 x^6 + 84 x^5 - 280 x^4 + 560 x^3 - 672 x^2 + 448 x - 128',
+      1,
+    ),
+    complicatedEquation().scale(1, 1),
+  );
+
+  yield* beginSlide('random algorithm example');
+
+  yield* all(
+    complicatedEquation().tex(
+      'x{{^4}} {{+ 3}}x {{- 1}} = (x{{^2}}  + x{{ + 1)(}}x{{^2}} {{- 1)}}',
+      1,
+    ),
+    complicatedEquation().scale(1.5, 1),
+  );
+
+  yield* beginSlide('random algorithm example p2');
+  yield* all(complicatedText().text('Try x = 2', 1), complicatedText().opacity(1, 1));
+
+  yield* waitFor(1);
+
+  yield* complicatedEquation().tex(
+    '2{{^4}} {{+ 3}} \\cdot 2 {{- 1}} {{=}} (2{{^2}} + 2 {{+ 1)(}}2{{^2}} {{- 1)}}',
+    1,
+  );
+
+  yield* waitFor(1);
+
+  yield* all(
+    complicatedEquation().tex('21 {{=}} 21', 1),
+    complicatedText().fill(Solarized.green, 1),
+    complicatedText().text('Success', 1),
+  );
+
+  yield* waitFor(1);
+
+  yield* beginSlide('random algorithm example, plug 3');
+
+  yield* all(
+    complicatedEquation().tex(
+      'x{{^4}} {{+ 3}}x {{- 1}} = (x{{^2}}  + x{{ + 1)(}}x{{^2}} {{- 1)}}',
+      1,
+    ),
+    complicatedText().text('Try x = 3', 1),
+    complicatedText().fill(Solarized.blue, 1),
+  );
+
+  yield* beginSlide('random algorithm example, plug 3, p2');
+
+  yield* all(
+    complicatedEquation().tex(
+      '3{{^4}} {{+ 3}}\\cdot 3 {{- 1}} = (3{{^2}}  + 3 {{+ 1)(}}3{{^2}} {{- 1)}}',
+      1,
+    ),
+    complicatedText().text('Try x = 3', 1),
+    complicatedText().fill(Solarized.blue, 1),
+  );
+
+  yield* waitFor(1);
+
+  yield* all(
+    complicatedEquation().tex('89 {{\\neq}} 104', 1),
+    complicatedText().fill(Solarized.red, 1),
+    complicatedText().text('Nope', 1),
+  );
+
+  yield* beginSlide('picking random is ok');
+
+  const desmosPlot = createRef<Img>();
+  camera.add(
+    <Img
+      ref={desmosPlot}
+      src={desmosPlotWrongEquation}
+      scale={0.75}
+      position={[0, 100]}
+      opacity={0}
+    />,
+  );
+  yield* desmosPlot().opacity(1, 1);
+
+  yield* beginSlide('equation algorithm');
+
+  yield* all(
+    desmosPlot().opacity(0, 1),
+    complicatedEquation().tex(
+      'x{{^4}} {{+ 3}}x {{- 1}} = (x{{^2}}  + x{{ + 1)(}}x{{^2}} {{- 1)}}',
+      0.001,
+    ),
+    complicatedText().opacity(0, 0.001),
+  );
+
+  const algorithmCode = createRef<Code>();
 
   // Julia code component
-  view.add(
+  camera.add(
     <Code
-      ref={juliaCode}
+      ref={algorithmCode}
       highlighter={juliaHighlighter}
       fontSize={32}
       position={[0, 180]}
@@ -132,26 +294,28 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    juliaCode().code(
-      `for n in 1:10000
-    lhs = 2 * sum((1:n).^7)
-    rhs = sum(1:n)^2 - 3*sum((1:n).^2)^2 + 4*sum((1:n).^3)^2
+    algorithmCode().code(
+      `for i in range(10):
+    x = rand()
+    lhs = x**4 + 3*x - 1
+    rhs = (x**2 + x + 1)*(x**2 - 1)
+    if lhs != rhs:
+        return False
 
-    @assert lhs == rhs
-end`,
+return True`,
       1,
     ),
-    complexLayout().position(complexLayout().position().addY(-150), 1),
+    complicatedLayout().position(complicatedLayout().position().addY(-150), 1),
   );
 
   const highlightRectangle = createRef<Rect>();
 
-  view.add(
+  camera.add(
     <Rect
       ref={highlightRectangle}
-      position={juliaCode().position()}
-      width={juliaCode().cacheBBox().expand(20).width}
-      height={juliaCode().cacheBBox().expand(20).height}
+      position={algorithmCode().position()}
+      width={algorithmCode().cacheBBox().expand(20).width}
+      height={algorithmCode().cacheBBox().expand(20).height}
       stroke={Solarized.gray}
       lineWidth={6}
       lineDash={[20, 10]}
@@ -168,14 +332,14 @@ end`,
   const highlightLoader = createRef<PolyTxt>();
   const signalCounter = createSignal(1);
 
-  view.add(
+  camera.add(
     <Layout
       layout
       direction={'row'}
       gap={20}
       alignItems={'center'}
       justifyContent={'end'}
-      right={[550, 260]}
+      right={[800, 260]}
       width={1} // done for right to work
       zIndex={1}
     >
@@ -203,7 +367,7 @@ end`,
     highlightN().opacity(0).opacity(1, 0.5),
     highlightCounter().opacity(0).opacity(1, 0.5),
     highlightLoader().opacity(0).opacity(0.5, 0.5),
-    delay(0.5, signalCounter(10000, 4, linear)),
+    delay(0.5, signalCounter(10, 4, linear)),
     all(
       highlightRectangle().stroke(Solarized.green, 4),
       highlightRectangle().fill(new Color(Solarized.green).alpha(0.15), 4),
@@ -230,174 +394,5 @@ end`,
 
   // === NEW SECTION: Trigonometric Identity ===
 
-  yield* waitFor(1);
-
-  // Fade out the complex equation and code
-  // yield* all(complexLayout().opacity(0, 1), juliaCode().opacity(0, 1));
-
-  // Create trigonometric equation layout
-  const trigLayout = createRef<Layout>();
-  const trigEquation = createRef<PolyLatex>();
-  const trigJuliaCode = createRef<Code>();
-
-  view.add(
-    <Layout
-      ref={trigLayout}
-      layout
-      direction={'column'}
-      gap={80}
-      alignItems={'center'}
-      opacity={0}
-      position={[0, -150]}
-    >
-      <PolyLatex
-        ref={trigEquation}
-        tex={
-          '\\frac{\\sin(x) + \\sin(3x) + \\sin(5x) + \\sin(7x)}{\\cos(x) + \\cos(3x) + \\cos(5x) + \\cos(7x)} = \\tan(4x)'
-        }
-        fontSize={55}
-      />
-    </Layout>,
-  );
-
-  yield* all(
-    complexLayout().position(complexLayout().position().addX(-1400), 1),
-    complexLayout().scale(0.5, 1),
-    juliaCode().position(juliaCode().position().addX(-1400), 1),
-    juliaCode().scale(0.5, 1),
-    trigLayout().position([1600, 0]).position([0, 0], 1),
-    trigLayout().scale(0.5).scale(1, 1),
-    delay(0.5, all(trigLayout().opacity(1, 0.1), trigEquation().write(2))),
-  );
-
-  yield* waitFor(1.5);
-
-  view.add(
-    <Code
-      ref={trigJuliaCode}
-      highlighter={juliaHighlighter}
-      fontSize={30}
-      position={[0, 180]}
-      opacity={0}
-    />,
-  );
-
-  yield* all(
-    trigJuliaCode().opacity(1, 0.5),
-    trigJuliaCode().code(
-      `using Random
-
-for i in 1:100_000
-    x = big(rand())
-
-    lhs = (sin(x) + sin(3x) + sin(5x) + sin(7x)) / (cos(x) + cos(3x) + cos(5x) + cos(7x))
-    rhs = tan(4x)
-
-    @assert abs(lhs - rhs) < 1e-16
-end`,
-      2,
-    ),
-    trigLayout().position(trigLayout().position().addY(-230), 1),
-  );
-
-  // Create highlight rectangle for trig code
-  const trigHighlightRect = createRef<Rect>();
-
-  view.add(
-    <Rect
-      ref={trigHighlightRect}
-      position={trigJuliaCode().position()}
-      width={trigJuliaCode().cacheBBox().expand(20).width}
-      height={trigJuliaCode().cacheBBox().expand(20).height}
-      stroke={Solarized.gray}
-      lineWidth={6}
-      lineDash={[20, 10]}
-      opacity={0}
-      fill={new Color(Solarized.gray).alpha(0.15)}
-      zIndex={-1}
-    />,
-  );
-
-  yield loop(() => trigHighlightRect().lineDashOffset(-30, 0.2, linear).to(0, 0));
-
-  // Create display for random x values and counter
-  const randomXDisplay = createRef<PolyTxt>();
-  const trigLoader = createRef<PolyTxt>();
-  const xValueSignal = createSignal(0.0);
-
-  view.add(
-    <Layout
-      layout
-      direction={'column'}
-      gap={15}
-      alignItems={'end'}
-      justifyContent={'center'}
-      right={[790, 320]}
-      width={1}
-      zIndex={1}
-    >
-      <Layout layout direction={'row'} gap={15} alignItems={'center'}>
-        <PolyTxt
-          ref={randomXDisplay}
-          fontFamily={'monospace'}
-          text={() =>
-            `x = ${xValueSignal() > 0 ? '+' : '–'}${Math.abs(xValueSignal()).toFixed(4)}`
-          }
-          fill={Solarized.blue}
-          opacity={0}
-        />
-        <PolyTxt
-          ref={trigLoader}
-          fontFamily={'monospace'}
-          text={() => `↻`}
-          fontSize={60}
-          opacity={0}
-        />
-      </Layout>
-    </Layout>,
-  );
-
-  // Animate the verification with random values
-  yield loop(5, () => trigLoader().rotation(0).rotation(360, 1, linear));
-
-  let random = useRandom();
-
-  // Generate random-looking x values
-  yield* all(
-    trigHighlightRect().opacity(1, 0.5),
-    randomXDisplay().opacity(1, 0.5),
-    trigLoader().opacity(0.5, 0.5),
-    // Animate x values changing randomly
-    delay(
-      0.5,
-      loop(45, () =>
-        chain(xValueSignal(2 * (random.nextFloat() - 0.5), 0.01), waitFor(0.09)),
-      ),
-    ),
-    all(
-      trigHighlightRect().stroke(Solarized.green, 4.5),
-      trigHighlightRect().fill(new Color(Solarized.green).alpha(0.15), 4.5),
-    ),
-    delay(
-      5,
-      all(
-        randomXDisplay().text('success', 0.01),
-        randomXDisplay().fill(Solarized.green, 0.01),
-        trigLoader().text('✅️', 0),
-      ),
-    ),
-  );
-
-  yield* waitFor(2);
-
-  // Final fade out
-  yield* all(
-    trigHighlightRect().opacity(0, 1),
-    randomXDisplay().opacity(0, 1),
-    trigLoader().opacity(0, 1),
-    trigLayout().opacity(0, 1),
-    trigJuliaCode().opacity(0, 1),
-  );
-
-  yield* waitFor(1);
+  yield* beginSlide('equations end');
 });
